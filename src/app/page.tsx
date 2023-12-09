2fb3585db7cb38865a65b8496e25a315171474ec
toha <toha@anton.com>
2023-12-09 14:23:14 +0200

    worksabit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import { mongoDb } from "@/lib/mongodb";
+
 
 type Tab = "all" | "mailing" | "analytics";
 
 export default async function Page({params,searchParams}:{params:Tab,searchParams:String}) {
-  console.log(params,searchParams);
-  const db = await mongoDb;
+
+
   return <span>PlaceHolder</span>
   ;
 }
